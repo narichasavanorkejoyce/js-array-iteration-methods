ed049e675578bc90ee7bdf78e134aec3aecaab0d
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-03-27 17:05:12 -0400

    forEach method

--- bin/for-each.js
+++ bin/for-each.js
@@ -6,6 +6,10 @@ const forEach = require('../lib/for-each.js')
 
 const workingArray = examples.numbers.whole
 
+// this is a fat arrow function
+// take three formal parameters
+// then it does something
+// this is what becomes 'callback'
 forEach(workingArray, (e, i, a) => {
   console.log(e, i, a === workingArray)
 })
