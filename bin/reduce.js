e173f1cefd485e82ed3e3c597f7fe194f4096427
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-03-27 17:07:10 -0400

    Use reduce method to add values or find the max value

--- bin/reduce.js
+++ bin/reduce.js
@@ -4,8 +4,20 @@
 const examples = require('../lib/examples')
 const reduce = require('../lib/reduce.js')
 
-const workingArray = examples.numbers.whole
+// const workingArray = examples.numbers.whole
+
+const loadCities = examples.cities.loadCities
+
+loadCities( (cities) => {
+  const city reduce(cities, (p, c) => {
+    // need to convert the population string to a number (+)
+    if (+p.population > +c.population) return p
+    return c
+  })
+  console.log(city)
+})
 
 const result = reduce(workingArray, (p, c, i, a) => p + c)
+const result = reduce(workingArray, (p, c, i, a) => p > c ? p : c) // give max #
 
 console.log('reduce result', result)
